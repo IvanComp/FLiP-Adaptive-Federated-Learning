268daa8733bcde20fae447f1b64fe3e6ea65bfc8
LesLivia <baresi@baresi-vm-1.inside.dei.polimi.it>
2025-07-06 14:34:59 +0000

    primi risultati con 5 repliche

--- Docker/replication_script.sh
+++ Docker/replication_script.sh
@@ -1,2 +1,2 @@
-./run_experiments.sh 10 5high-5low/40iid no-selector
-./run_experiments.sh 10 5high-5low/40iid always-selector
+./run_experiments.sh 5 5high-5low/0iid no-selector
+./run_experiments.sh 5 5high-5low/0iid always-selector
