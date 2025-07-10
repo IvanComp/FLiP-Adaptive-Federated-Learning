2b7ab9f5b533e1e7e7527a57024b0775df102be4
LesLivia <baresi@baresi-vm-1.inside.dei.polimi.it>
2025-07-10 07:03:20 +0000

    new results

--- Docker/replication_script.sh
+++ Docker/replication_script.sh
@@ -1,8 +1,8 @@
-./run_experiments.sh --repl $1 --config_name no-selector --iid 100 --high 10 --low 10
-./run_experiments.sh --repl $1 --config_name fixed-adaptive --iid 100 --high 10 --low 10 --threshold 0.3
-./run_experiments.sh --repl $1 --config_name tree-adaptive --iid 100 --high 10 --low 10
-./run_experiments.sh --repl $1 --config_name always-selector --iid 100 --high 20 --low 0
-./run_experiments.sh --repl $1 --config_name no-selector --iid 0 --high 10 --low 10
-./run_experiments.sh --repl $1 --config_name fixed-adaptive --iid 0 --high 10 --low 10 --threshold 0.3
-./run_experiments.sh --repl $1 --config_name tree-adaptive --iid 0 --high 10 --low 10
-./run_experiments.sh --repl $1 --config_name always-selector --iid 0 --high 20 --low 0
+./run_experiments.sh --repl $1 --config_name no-selector --iid 100 --high 4 --low 2
+./run_experiments.sh --repl $1 --config_name fixed-adaptive --iid 100 --high 4 --low 2 --threshold 0.4
+./run_experiments.sh --repl $1 --config_name tree-adaptive --iid 100 --high 4 --low 2
+./run_experiments.sh --repl $1 --config_name always-selector --iid 100 --high 6 --low 0
+./run_experiments.sh --repl $1 --config_name no-selector --iid 0 --high 4 --low 2
+./run_experiments.sh --repl $1 --config_name fixed-adaptive --iid 0 --high 4 --low 2 --threshold 0.3
+./run_experiments.sh --repl $1 --config_name tree-adaptive --iid 0 --high 4 --low 2
+./run_experiments.sh --repl $1 --config_name always-selector --iid 0 --high 6 --low 0
