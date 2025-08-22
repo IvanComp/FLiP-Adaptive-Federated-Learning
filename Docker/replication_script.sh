3ad5caf3a0830ab88fc7f1ed3794093adee32bcd
LesLivia <baresi@baresi-vm-1.inside.dei.polimi.it>
2025-08-22 13:18:12 +0000

    new results

--- Docker/replication_script.sh
+++ Docker/replication_script.sh
@@ -1,7 +1,7 @@
-./run_experiments.sh --repl $1 --config_name never-selector --iid 0 --high 2 --low 4 --data new
-./run_experiments.sh --repl $1 --config_name never-selector --iid 0 --high 4 --low 2 --data new
-./run_experiments.sh --repl $1 --config_name never-selector --iid 0 --high 3 --low 3 --data new
-./run_experiments.sh --repl $1 --config_name never-selector --iid 0 --high 5 --low 5 --data new
+./run_experiments.sh --repl $1 --config_name no-selector --iid 0 --high 2 --low 4 --data new
+./run_experiments.sh --repl $1 --config_name no-selector --iid 0 --high 4 --low 2 --data new
+./run_experiments.sh --repl $1 --config_name no-selector --iid 0 --high 3 --low 3 --data new
+./run_experiments.sh --repl $1 --config_name no-selector --iid 0 --high 5 --low 5 --data new
 ./run_experiments.sh --repl $1 --config_name always-selector --iid 0 --high 2 --low 4 --data new
 ./run_experiments.sh --repl $1 --config_name always-selector --iid 0 --high 4 --low 2 --data new
 ./run_experiments.sh --repl $1 --config_name always-selector --iid 0 --high 3 --low 3 --data new
@@ -22,10 +22,10 @@
 ./run_experiments.sh --repl $1 --config_name bo-selector --iid 0 --high 4 --low 2 --data new
 ./run_experiments.sh --repl $1 --config_name bo-selector --iid 0 --high 3 --low 3 --data new
 ./run_experiments.sh --repl $1 --config_name bo-selector --iid 0 --high 5 --low 5 --data new
-./run_experiments.sh --repl $1 --config_name never-selector --iid 100 --high 2 --low 4 --data new
-./run_experiments.sh --repl $1 --config_name never-selector --iid 100 --high 4 --low 2 --data new
-./run_experiments.sh --repl $1 --config_name never-selector --iid 100 --high 3 --low 3 --data new
-./run_experiments.sh --repl $1 --config_name never-selector --iid 100 --high 5 --low 5 --data new
+./run_experiments.sh --repl $1 --config_name no-selector --iid 100 --high 2 --low 4 --data new
+./run_experiments.sh --repl $1 --config_name no-selector --iid 100 --high 4 --low 2 --data new
+./run_experiments.sh --repl $1 --config_name no-selector --iid 100 --high 3 --low 3 --data new
+./run_experiments.sh --repl $1 --config_name no-selector --iid 100 --high 5 --low 5 --data new
 ./run_experiments.sh --repl $1 --config_name always-selector --iid 100 --high 2 --low 4 --data new
 ./run_experiments.sh --repl $1 --config_name always-selector --iid 100 --high 4 --low 2 --data new
 ./run_experiments.sh --repl $1 --config_name always-selector --iid 100 --high 3 --low 3 --data new
