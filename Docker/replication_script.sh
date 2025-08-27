6104dd2f5281f591e7847a896e5859aa95fe75ae
LesLivia <baresi@baresi-vm-1.inside.dei.polimi.it>
2025-08-27 08:13:29 +0000

    new results

--- Docker/replication_script.sh
+++ Docker/replication_script.sh
@@ -1,1 +1,4 @@
-./run_experiments.sh --repl $1 --config_name fixed-selector --iid 0 --high 10 --low 10 --data new --threshold 0.4
+./run_experiments.sh --repl $1 --config_name tree-selector --iid 100 --high 2 --low 4 --data new
+./run_experiments.sh --repl $1 --config_name tree-selector --iid 100 --high 4 --low 2 --data new
+./run_experiments.sh --repl $1 --config_name tree-selector --iid 100 --high 3 --low 3 --data new
+./run_experiments.sh --repl $1 --config_name tree-selector --iid 100 --high 5 --low 5 --data new
