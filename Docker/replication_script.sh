d244a7244dafa31bcaa7c6905f9b0df2bf673e6c
LesLivia <baresi@baresi-vm-1.inside.dei.polimi.it>
2025-08-29 15:44:16 +0000

    new results

--- Docker/replication_script.sh
+++ Docker/replication_script.sh
@@ -1,6 +1,32 @@
-./run_experiments.sh --repl $1 --config_name tree-selector --iid 0 --high 2 --low 8 --data new
-./run_experiments.sh --repl $1 --config_name tree-selector --iid 0 --high 4 --low 8 --data new
-./run_experiments.sh --repl $1 --config_name tree-selector --iid 0 --high 8 --low 4 --data new
-./run_experiments.sh --repl $1 --config_name tree-selector --iid 100 --high 2 --low 8 --data new
-./run_experiments.sh --repl $1 --config_name tree-selector --iid 100 --high 4 --low 8 --data new
-./run_experiments.sh --repl $1 --config_name tree-selector --iid 100 --high 8 --low 4 --data new
+./run_experiments.sh --repl $1 --config_name tree-selector --iid 0 --high 2 --low 4 --data new
+./run_experiments.sh --repl $1 --config_name tree-selector --iid 100 --high 2 --low 4 --data new
+./run_experiments.sh --repl $1 --config_name always-selector --iid 0 --high 2 --low 8 --data new
+./run_experiments.sh --repl $1 --config_name always-selector --iid 100 --high 2 --low 8 --data new
+./run_experiments.sh --repl $1 --config_name fixed-selector --iid 0 --high 2 --low 8 --data new --threshold 0.2
+./run_experiments.sh --repl $1 --config_name fixed-selector --iid 100 --high 2 --low 8 --data new --threshold 0.4
+./run_experiments.sh --repl $1 --config_name no-selector --iid 0 --high 2 --low 8 --data new
+./run_experiments.sh --repl $1 --config_name no-selector --iid 100 --high 2 --low 8 --data new
+./run_experiments.sh --repl $1 --config_name random-selector --iid 0 --high 2 --low 8 --data new
+./run_experiments.sh --repl $1 --config_name random-selector --iid 100 --high 2 --low 8 --data new
+./run_experiments.sh --repl $1 --config_name tree-selector --iid 0 --high 3 --low 3 --data new
+./run_experiments.sh --repl $1 --config_name tree-selector --iid 100 --high 3 --low 3 --data new
+./run_experiments.sh --repl $1 --config_name tree-selector --iid 0 --high 4 --low 2 --data new
+./run_experiments.sh --repl $1 --config_name tree-selector --iid 100 --high 4 --low 2 --data new
+./run_experiments.sh --repl $1 --config_name always-selector --iid 0 --high 4 --low 8 --data new
+./run_experiments.sh --repl $1 --config_name always-selector --iid 100 --high 4 --low 8 --data new
+./run_experiments.sh --repl $1 --config_name fixed-selector --iid 0 --high 4 --low 8 --data new --threshold 0.2
+./run_experiments.sh --repl $1 --config_name fixed-selector --iid 100 --high 4 --low 8 --data new --threshold 0.4
+./run_experiments.sh --repl $1 --config_name no-selector --iid 0 --high 4 --low 8 --data new
+./run_experiments.sh --repl $1 --config_name no-selector --iid 100 --high 4 --low 8 --data new
+./run_experiments.sh --repl $1 --config_name random-selector --iid 0 --high 4 --low 8 --data new
+./run_experiments.sh --repl $1 --config_name random-selector --iid 100 --high 4 --low 8 --data new
+./run_experiments.sh --repl $1 --config_name tree-selector --iid 0 --high 5 --low 5 --data new
+./run_experiments.sh --repl $1 --config_name tree-selector --iid 100 --high 5 --low 5 --data new
+./run_experiments.sh --repl $1 --config_name always-selector --iid 0 --high 8 --low 4 --data new
+./run_experiments.sh --repl $1 --config_name always-selector --iid 100 --high 8 --low 4 --data new
+./run_experiments.sh --repl $1 --config_name fixed-selector --iid 0 --high 8 --low 4 --data new --threshold 0.2
+./run_experiments.sh --repl $1 --config_name fixed-selector --iid 100 --high 8 --low 4 --data new --threshold 0.4
+./run_experiments.sh --repl $1 --config_name no-selector --iid 0 --high 8 --low 4 --data new
+./run_experiments.sh --repl $1 --config_name no-selector --iid 100 --high 8 --low 4 --data new
+./run_experiments.sh --repl $1 --config_name random-selector --iid 0 --high 8 --low 4 --data new
+./run_experiments.sh --repl $1 --config_name random-selector --iid 100 --high 8 --low 4 --data new
