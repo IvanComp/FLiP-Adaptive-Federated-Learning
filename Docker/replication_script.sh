3d72f11411c690ffcc8168f83c083230c84944c9
LesLivia <baresi@baresi-vm-1.inside.dei.polimi.it>
2025-09-12 09:55:59 +0000

    new results

--- Docker/replication_script.sh
+++ Docker/replication_script.sh
@@ -1,1 +1,4 @@
 ./run_experiments.sh --repl $1 --config_name bo-selector --iid 0 --high 2 --low 4 --data new
+./run_experiments.sh --repl $1 --config_name bo-selector --iid 0 --high 3 --low 3 --data new
+./run_experiments.sh --repl $1 --config_name bo-selector --iid 0 --high 4 --low 2 --data new
+./run_experiments.sh --repl $1 --config_name bo-selector --iid 0 --high 5 --low 5 --data new
