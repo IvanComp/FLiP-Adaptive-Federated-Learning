4a8b4100202a3ff0805a0e58bcf6dcac20c58a3c
LesLivia <baresi@baresi-vm-1.inside.dei.polimi.it>
2025-09-18 11:06:10 +0000

    new results

--- Docker/replication_script.sh
+++ Docker/replication_script.sh
@@ -1,24 +1,7 @@
-./run_experiments.sh --repl $1 --config_name no-hdh --iid 0 --high 2 --low 4 --data new
+./run_experiments.sh --repl $1 --config_name fixed-hdh --iid 0 --high 2 --low 4 --data new --threshold 0.1
 ./run_experiments.sh --repl $1 --config_name always-hdh --iid 0 --high 2 --low 4 --data new
-./run_experiments.sh --repl $1 --config_name random-hdh --iid 0 --high 2 --low 4 --data new
-./run_experiments.sh --repl $1 --config_name no-hdh --iid 100 --high 2 --low 4 --data new
-./run_experiments.sh --repl $1 --config_name always-hdh --iid 100 --high 2 --low 4 --data new
-./run_experiments.sh --repl $1 --config_name random-hdh --iid 100 --high 2 --low 4 --data new
-./run_experiments.sh --repl $1 --config_name no-hdh --iid 0 --high 3 --low 3 --data new
-./run_experiments.sh --repl $1 --config_name always-hdh --iid 0 --high 3 --low 3 --data new
-./run_experiments.sh --repl $1 --config_name random-hdh --iid 0 --high 3 --low 3 --data new
-./run_experiments.sh --repl $1 --config_name no-hdh --iid 100 --high 3 --low 3 --data new
-./run_experiments.sh --repl $1 --config_name always-hdh --iid 100 --high 3 --low 3 --data new
-./run_experiments.sh --repl $1 --config_name random-hdh --iid 100 --high 3 --low 3 --data new
-./run_experiments.sh --repl $1 --config_name no-hdh --iid 0 --high 4 --low 2 --data new
+./run_experiments.sh --repl $1 --config_name fixed-hdh --iid 0 --high 3 --low 3 --data new --threshold 0.1
+./run_experiments.sh --repl $1 --config_name always-hdh --iid 0 --high 3 --low 3 --data new 
+./run_experiments.sh --repl $1 --config_name fixed-hdh --iid 0 --high 4 --low 2 --data new --threshold 0.1
 ./run_experiments.sh --repl $1 --config_name always-hdh --iid 0 --high 4 --low 2 --data new
-./run_experiments.sh --repl $1 --config_name random-hdh --iid 0 --high 4 --low 2 --data new
-./run_experiments.sh --repl $1 --config_name no-hdh --iid 100 --high 4 --low 2 --data new
-./run_experiments.sh --repl $1 --config_name always-hdh --iid 100 --high 4 --low 2 --data new
-./run_experiments.sh --repl $1 --config_name random-hdh --iid 100 --high 4 --low 2 --data new
-./run_experiments.sh --repl $1 --config_name no-hdh --iid 0 --high 5 --low 5 --data new
-./run_experiments.sh --repl $1 --config_name always-hdh --iid 0 --high 5 --low 5 --data new
-./run_experiments.sh --repl $1 --config_name random-hdh --iid 0 --high 5 --low 5 --data new
-./run_experiments.sh --repl $1 --config_name no-hdh --iid 100 --high 5 --low 5 --data new
-./run_experiments.sh --repl $1 --config_name always-hdh --iid 100 --high 5 --low 5 --data new
-./run_experiments.sh --repl $1 --config_name random-hdh --iid 100 --high 5 --low 5 --data new
+./run_experiments.sh --repl $1 --config_name fixed-hdh --iid 0 --high 5 --low 5 --data new --threshold 0.1
