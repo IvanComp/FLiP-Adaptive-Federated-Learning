cb10d12774e691e1e7d5f184fede6980c80a12d0
LesLivia <baresi@baresi-vm-1.inside.dei.polimi.it>
2025-09-19 06:37:27 +0000

    new results

--- Docker/replication_script.sh
+++ Docker/replication_script.sh
@@ -1,7 +1,4 @@
-./run_experiments.sh --repl $1 --config_name fixed-hdh --iid 0 --high 2 --low 4 --data new --threshold 0.1
-./run_experiments.sh --repl $1 --config_name always-hdh --iid 0 --high 2 --low 4 --data new
-./run_experiments.sh --repl $1 --config_name fixed-hdh --iid 0 --high 3 --low 3 --data new --threshold 0.1
-./run_experiments.sh --repl $1 --config_name always-hdh --iid 0 --high 3 --low 3 --data new 
-./run_experiments.sh --repl $1 --config_name fixed-hdh --iid 0 --high 4 --low 2 --data new --threshold 0.1
-./run_experiments.sh --repl $1 --config_name always-hdh --iid 0 --high 4 --low 2 --data new
-./run_experiments.sh --repl $1 --config_name fixed-hdh --iid 0 --high 5 --low 5 --data new --threshold 0.1
+./run_experiments.sh --repl $1 --config_name tree-hdh --iid 0 --high 2 --low 4 --data new
+./run_experiments.sh --repl $1 --config_name tree-hdh --iid 0 --high 3 --low 3 --data new 
+./run_experiments.sh --repl $1 --config_name tree-hdh --iid 0 --high 4 --low 2 --data new
+./run_experiments.sh --repl $1 --config_name tree-hdh --iid 0 --high 5 --low 5 --data new
