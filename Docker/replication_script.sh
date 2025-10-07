7698705df33097ce250e5fb0b461f8c0ce637a22
LesLivia <baresi@baresi-vm-1.inside.dei.polimi.it>
2025-10-07 06:59:04 +0000

    new results

--- Docker/replication_script.sh
+++ Docker/replication_script.sh
@@ -43,3 +43,45 @@
 ./run_experiments.sh --repl $1 --config_name fixed-hdh --iid 0 --high 8 --low 4 --data same --threshold 0.1
 ./run_experiments.sh --repl $1 --config_name tree-hdh --iid 0 --high 8 --low 4 --data same
 ./run_experiments.sh --repl $1 --config_name bo-hdh --iid 0 --high 8 --low 4 --data same
+./run_experiments.sh --repl $1 --config_name no-hdh --iid 0 --high 2 --low 4 --data new
+./run_experiments.sh --repl $1 --config_name random-hdh --iid 0 --high 2 --low 4 --data new
+./run_experiments.sh --repl $1 --config_name always-hdh --iid 0 --high 2 --low 4 --data new
+./run_experiments.sh --repl $1 --config_name fixed-hdh --iid 0 --high 2 --low 4 --data new --threshold 0.1
+./run_experiments.sh --repl $1 --config_name tree-hdh --iid 0 --high 2 --low 4 --data new
+./run_experiments.sh --repl $1 --config_name bo-hdh --iid 0 --high 2 --low 4 --data new
+./run_experiments.sh --repl $1 --config_name no-hdh --iid 0 --high 3 --low 3 --data new
+./run_experiments.sh --repl $1 --config_name random-hdh --iid 0 --high 3 --low 3 --data new
+./run_experiments.sh --repl $1 --config_name always-hdh --iid 0 --high 3 --low 3 --data new
+./run_experiments.sh --repl $1 --config_name fixed-hdh --iid 0 --high 3 --low 3 --data new --threshold 0.1
+./run_experiments.sh --repl $1 --config_name tree-hdh --iid 0 --high 3 --low 3 --data new
+./run_experiments.sh --repl $1 --config_name bo-hdh --iid 0 --high 3 --low 3 --data new
+./run_experiments.sh --repl $1 --config_name no-hdh --iid 0 --high 4 --low 2 --data new
+./run_experiments.sh --repl $1 --config_name random-hdh --iid 0 --high 4 --low 2 --data new
+./run_experiments.sh --repl $1 --config_name always-hdh --iid 0 --high 4 --low 2 --data new
+./run_experiments.sh --repl $1 --config_name fixed-hdh --iid 0 --high 4 --low 2 --data new --threshold 0.1
+./run_experiments.sh --repl $1 --config_name tree-hdh --iid 0 --high 4 --low 2 --data new
+./run_experiments.sh --repl $1 --config_name bo-hdh --iid 0 --high 4 --low 2 --data new
+./run_experiments.sh --repl $1 --config_name no-hdh --iid 0 --high 5 --low 5 --data new
+./run_experiments.sh --repl $1 --config_name random-hdh --iid 0 --high 5 --low 5 --data new
+./run_experiments.sh --repl $1 --config_name always-hdh --iid 0 --high 5 --low 5 --data new
+./run_experiments.sh --repl $1 --config_name fixed-hdh --iid 0 --high 5 --low 5 --data new --threshold 0.1
+./run_experiments.sh --repl $1 --config_name tree-hdh --iid 0 --high 5 --low 5 --data new
+./run_experiments.sh --repl $1 --config_name bo-hdh --iid 0 --high 5 --low 5 --data new
+./run_experiments.sh --repl $1 --config_name no-hdh --iid 0 --high 2 --low 8 --data new
+./run_experiments.sh --repl $1 --config_name random-hdh --iid 0 --high 2 --low 8 --data new
+./run_experiments.sh --repl $1 --config_name always-hdh --iid 0 --high 2 --low 8 --data new
+./run_experiments.sh --repl $1 --config_name fixed-hdh --iid 0 --high 2 --low 8 --data new --threshold 0.1
+./run_experiments.sh --repl $1 --config_name tree-hdh --iid 0 --high 2 --low 8 --data new
+./run_experiments.sh --repl $1 --config_name bo-hdh --iid 0 --high 2 --low 8 --data new
+./run_experiments.sh --repl $1 --config_name no-hdh --iid 0 --high 4 --low 8 --data new
+./run_experiments.sh --repl $1 --config_name random-hdh --iid 0 --high 4 --low 8 --data new
+./run_experiments.sh --repl $1 --config_name always-hdh --iid 0 --high 4 --low 8 --data new
+./run_experiments.sh --repl $1 --config_name fixed-hdh --iid 0 --high 4 --low 8 --data new --threshold 0.1
+./run_experiments.sh --repl $1 --config_name tree-hdh --iid 0 --high 4 --low 8 --data new
+./run_experiments.sh --repl $1 --config_name bo-hdh --iid 0 --high 4 --low 8 --data new
+./run_experiments.sh --repl $1 --config_name no-hdh --iid 0 --high 8 --low 4 --data new
+./run_experiments.sh --repl $1 --config_name random-hdh --iid 0 --high 8 --low 4 --data new
+./run_experiments.sh --repl $1 --config_name always-hdh --iid 0 --high 8 --low 4 --data new
+./run_experiments.sh --repl $1 --config_name fixed-hdh --iid 0 --high 8 --low 4 --data new --threshold 0.1
+./run_experiments.sh --repl $1 --config_name tree-hdh --iid 0 --high 8 --low 4 --data new
+./run_experiments.sh --repl $1 --config_name bo-hdh --iid 0 --high 8 --low 4 --data new
